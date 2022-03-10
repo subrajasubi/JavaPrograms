a7f446f62a455e400829901e67250ad4060ddde0
subrajasubi <95363799+subrajasubi@users.noreply.github.com>
2022-03-10 13:21:01 +0530

    Add files via upload

---
+++ SecondLargestNumber.java
@@ -0,0 +1,16 @@
+package javaprograms;
+
+import java.util.Arrays;
+
+public class SecondLargestNumber {
+
+	public static void main(String[] args) {
+		int[] data = {3,2,11,4,6,7};
+		Arrays.sort(data);
+		System.out.println("The Second Largest Number is : ");
+		System.out.println(data[data.length-2]);
+		
+
+	}
+
+}
